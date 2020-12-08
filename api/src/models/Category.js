04acaafb27d62dd99e4e3c697bb1f5378f0f406e
Le127 <nereocarbonara@gmail.com>
2020-12-08 18:27:56 -0300

    Category model

---
+++ api/src/models/Category.js
@@ -0,0 +1,9 @@
+import Sequalize from "sequelize";
+
+const Category = db.define("category", {
+    name: {
+        type: Sequalize.STRING,
+        allowNull: false
+    }
+})
+export default Category;
