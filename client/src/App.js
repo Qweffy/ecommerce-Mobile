7b17292a632866ad02e5eee2def017ff8e61ee7c
Juan Ceschin <juanceschin@gmail.com>
2020-12-09 15:34:55 -0300

    Add client/App.jsx: create catalogue route

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import CreateCategory from "./Components/CRUDcategory/CreateCategory.jsx";
 import Productos from "./Components/Productos/Productos";
 import CreateProduct from "./Components/CRUDproduct/CreateProduct.jsx";
+import Catalogue from "./Components/Catalogue/Catalogue.jsx";
 
 function App() {
   return (
     <Router>
       <Route exact path="/createproduct" component={CreateProduct} />
       <Route exact path="/createcategory" component={CreateCategory} />
+      <Route exact path="/catalogue" component={Catalogue} />
     </Router>
   );
 }
