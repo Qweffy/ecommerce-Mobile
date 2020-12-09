94f5f2c20adea9d2bf4eb158ac90393fe29b9391
Juan Ceschin <juanceschin@gmail.com>
2020-12-09 15:25:31 -0300

    Add client/src/components/ProductCard.jsx: create main layout

---
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+const ProductCard = ({ img, name, price }) => {
+    const state = useSelector(state => state);
+
+    return (
+        <div className="d-flex">
+            <img src="" alt="Img not found"/>
+            <div className="product-info">
+                <h3>{name}</h3>
+                <p>Insert stars!!</p>
+                <p>{price}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
