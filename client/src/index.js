e868016c13b3a7da55a46eb5bbce5e8feef578a8
Nicolas Valencia <nicolasv0415@gmail.com>
2020-12-09 10:45:18 -0500

    Utilizar React Router en App

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 
+
 import App from './App';
 import { st } from './store/index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={st}>
-      <App />
-    </Provider>
+      <Provider store={st}>
+        <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
