3490e719aabaa9f38648cdffa51c65d1debd7ebf
Le127 <nereocarbonara@gmail.com>
2020-12-09 10:04:40 -0300

    Correccion y creacion ProductCategory

--- api/src/db.js
+++ api/src/db.js
@@ -36,6 +36,7 @@ const { Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Category.belongsToMany(Products, { through: Product_Category });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
