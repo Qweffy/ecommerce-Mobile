f401de490781f3af2736b161927199bcb6df5880
Sloot04 <suyaislootmans@gmail.com>
2020-12-10 21:48:51 -0300

    filtro por categoria back, falta front

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,26 +1,40 @@
 const server = require("express").Router();
-const { Product } = require("../db.js");
+const { Product, Category } = require("../db.js");
 
 //Create new product ----> '/products'
 server.post('/', (req, res, next) => {
 
   const { name, description, price, stock, img } = req.body
 
-  return Product.create({name, description, price, stock, img})
-                .then(product =>{
-                  res.json({mensaje: 'producto creado OK', data: product})
-                })
-                .catch( err => {
-                  res.json({mensaje: 'Error al creat el producto', data: err})
-                })
-  });
+  return Product.create({ name, description, price, stock, img })
+    .then(product => {
+      res.json({ mensaje: 'producto creado OK', data: product })
+    })
+    .catch(err => {
+      res.json({ mensaje: 'Error al creat el producto', data: err })
+    })
+});
 
 // get an all products ----> '/products'
 server.get("/", (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.send(products);
+  let categories = req.query.categories;
+  let q;
+  if (typeof categories === 'undefined' || categories === []) {
+    q = Product.findAll();
+  } else {
+    q = Product.findAll({
+      include: {
+        model: Category,
+        required: true,
+        where: {
+          name: req.query.categories
+        }
+      }
     })
+  }
+  q.then((products) => {
+    res.send(products);
+  })
     .catch(next);
 });
 
@@ -29,39 +43,39 @@ server.post('/:idProd/category/:idCateg', (req, res, next) => {
 }); // carga categoria a los productos
 
 //Modify an especific product ---> '/products/:id'
-server.put('/:id', (req, res)=>{
+server.put('/:id', (req, res) => {
   //sacamos el id del producto que queremos modificar
   const { id } = req.params;
   //del body sacamos los datos que queremos modificar
   const { name, description, price, stock, img } = req.body
 
-  return Product.findOne({ where: { id }})
-                .then( product => { //pisamos las propiedades del producto que encontramos
-                  product.name = name;
-                  product.description = description;
-                  product.price = price;
-                  product.stock = stock;
-                  product.img = img;
-                  product.save();
-                  res.status(200).json({mensaje: 'Se modifivo con exito', data: product})
-                })
-                .catch( err => {
-                  res.status(400).json({mensaje: 'Los campos enviados no son correctos', data: err})
-                })
-} )
+  return Product.findOne({ where: { id } })
+    .then(product => { //pisamos las propiedades del producto que encontramos
+      product.name = name;
+      product.description = description;
+      product.price = price;
+      product.stock = stock;
+      product.img = img;
+      product.save();
+      res.status(200).json({ mensaje: 'Se modifivo con exito', data: product })
+    })
+    .catch(err => {
+      res.status(400).json({ mensaje: 'Los campos enviados no son correctos', data: err })
+    })
+})
 
 //Delete an especific product -----> '/products/:id'
-server.delete('/:id', ( req, res )=>{
+server.delete('/:id', (req, res) => {
   const { id } = req.params;
 
-  return Product.findOne({ where: { id }})
-                .then(deleteProduct => {
-                  deleteProduct.destroy();
-                  res.status(200).json({mensaje: 'El producto fue eliminado correctamente', data: deleteProduct});
-                })
-                .catch(err => {
-                  res.status(400).json({mensaje: 'No se pudo eliminar el producto', data: err});
-                })
+  return Product.findOne({ where: { id } })
+    .then(deleteProduct => {
+      deleteProduct.destroy();
+      res.status(200).json({ mensaje: 'El producto fue eliminado correctamente', data: deleteProduct });
+    })
+    .catch(err => {
+      res.status(400).json({ mensaje: 'No se pudo eliminar el producto', data: err });
+    })
 })
 
 server.get("/search", (req, res) => {
