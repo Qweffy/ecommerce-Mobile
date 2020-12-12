11ab185328bd4a5ecc45bfccf7fca48d1fe6ae2d
Juan Ceschin <juanceschin@gmail.com>
2020-12-12 15:16:48 -0300

    Updated client/src/Components/ProductCard/ProductCard.css & ProductCard.jsx: changed bg color and borders of cards

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({product}) => {
     const { name, price, img } = product;
 
     return (
-        <div className="product-card border rounded p-2">
+        <div className="product-card p-2">
             <img src={img} alt="img not found" className="product-img"/>
             <div className="product-info p-2 mt-4">
                 <h5>{name}</h5>
