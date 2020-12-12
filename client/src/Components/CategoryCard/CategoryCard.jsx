58647d4e7b119c867f645bf795644d165c813999
Sloot04 <suyaislootmans@gmail.com>
2020-12-12 01:08:31 -0300

    update (client/src/Components/CategoryCard/CategoryCard.jsx) added checkbox

--- client/src/Components/CategoryCard/CategoryCard.jsx
+++ client/src/Components/CategoryCard/CategoryCard.jsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import './CategoryCard.css';
 
 const CategoryCard = ({ categories, onCategoryToggle }) => {
-   
+
     return (
         <div className="mx-5 px-1 py-3 justify-content-center">
             <h4>Categories</h4>
             {
                 categories.map(category => {
                     return (
-                        <button onClick={() => onCategoryToggle(category.name)} className="border-bottom p-1">{category.name}</button>
-
+                        <label class="checkbox-inline">
+                            <input type="checkbox" 
+                            onClick={() => onCategoryToggle(category.name)} 
+                            data-toggle="toggle" />{category.name}
+                        </label>
                     )
                 })
             }
