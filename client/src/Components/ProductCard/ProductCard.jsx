f1b95cc851ef06300758a012cbd7e130f0976759
Juan Ceschin <juanceschin@gmail.com>
2020-12-12 15:46:32 -0300

    Updated client/src/Components/ProductCard/ProductCard.css & ProductCard.jsx: improved cards style

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -5,11 +5,11 @@ const ProductCard = ({product}) => {
     const { name, price, img } = product;
 
     return (
-        <div className="product-card p-2">
+        <div className="product-card pb-2 pt-3 px-1">
             <img src={img} alt="img not found" className="product-img"/>
             <div className="product-info p-2 mt-4">
-                <h5>{name}</h5>
-                <p>${price}</p>
+                <p>{name}</p>
+                <h5 className="product-price"><strong>${price}</strong></h5>
             </div>
         </div>
     );
