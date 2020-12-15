4d04c9baf622808cf2bca7e5b9b16d6bf4021ad7
Le127 <nereocarbonara@gmail.com>
2020-12-15 00:26:30 -0300

    Creacion orden y rutas   Components/Order  routes/order.js

---
+++ client/src/Components/Order/Order.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default Order = () => {
+    return (
+        <div>
+            <div>
+                <p>Productos comprados:</p> 
+                <p>Precio unidad :</p>
+            </div>
+            <div>
+                <p>Total:</p>
+            </div>
+        </div>
+    )
+}
