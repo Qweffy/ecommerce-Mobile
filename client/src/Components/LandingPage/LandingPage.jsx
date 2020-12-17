a8d7161af3f1a30399a07d9ee9b4ec7e1f908545
Juan Ceschin <juanceschin@gmail.com>
2020-12-17 15:13:46 -0300

    Updated Components/Landingpage.jsx: added products grid below form

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductCard from "../ProductCard/ProductCard.jsx";
 import Navbar from '../Navbar/Navbar.jsx';
 import Banner from './Banner/Banner.jsx';
 
 const LandingPage = () => {
+    const [products, setProducts] = useState([]);
+
+    async function getProducts() {
+        let response = await axios.get("http://localhost:4000/products/");
+        setProducts(response.data);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
     return (
         <div>
             <Navbar />
             <Banner />
+            <div className="products-grid m-5">
+                {
+                    products.map((product, index) => {
+                        return (
+                            <div key={index}>
+                            <ProductCard product={product} />
+                            </div>
+                        );
+                    })
+                }
+            </div>            
         </div>
     )
 };
