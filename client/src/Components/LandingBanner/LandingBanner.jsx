143f2a3018f738282481f7d2205cffa069b84204
Juan Ceschin <juanceschin@gmail.com>
2020-12-17 11:30:52 -0300

    Created client/src/Components/LandingBanner: defined as LandingPage child

---
+++ client/src/Components/LandingBanner/LandingBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const LandingBanner = () => {
+};
+
+export default LandingBanner;
--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from '../Navbar/Navbar.jsx';
+import LandingBanner from '../LandingBanner/LandingBanner.jsx';
 
 const LandingPage = () => {
     return (
         <div>
             <Navbar />
+            <LandingBanner />
         </div>
     )
 };
