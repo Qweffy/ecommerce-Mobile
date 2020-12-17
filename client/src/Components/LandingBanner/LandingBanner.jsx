96a80a91b0dc1ccb72291f806c2150cc796c96d6
Juan Ceschin <juanceschin@gmail.com>
2020-12-17 11:41:26 -0300

    Updated LandingBanner.jsx & .css: added banner

--- client/src/Components/LandingBanner/LandingBanner.jsx
+++ client/src/Components/LandingBanner/LandingBanner.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import './LandingBanner.css';
 
 
 const LandingBanner = () => {
+    return (
+        <div className="banner"></div>
+    )
 };
 
 export default LandingBanner;
