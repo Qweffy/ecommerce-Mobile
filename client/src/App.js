1c4d1d3cab6cda019488f26ab6a75cd54aefc8f9
Nico Mastakas <nicolasmastakas@gmail.com>
2021-01-03 20:29:36 -0300

    asdasd

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ function App() {
       <Route exact path="/createcategory" component={CreateCategory} />
       <Route exact path="/createuser" component={CreateUser} />
       <Route exact path="/catalogue" component={Catalogue} />
+      <Route exact path="/userlist" component={userLIST} />
       <Route exact path="/catalogue/:search" component={Catalogue} />
       <Route exact path="/products/:id" component={Producto} />
       <Route exact path="/cart" component={Cart} />
