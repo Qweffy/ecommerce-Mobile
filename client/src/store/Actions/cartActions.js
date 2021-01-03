0bd9517f951a6e16256cc3825c88309ce044ac19
Nico Mastakas <nicolasmastakas@gmail.com>
2021-01-03 20:42:18 -0300

    hola

---
+++ client/src/store/Actions/cartActions.js
@@ -0,0 +1,56 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  FETCH_LOCAL_CART,
+  UPDATE_COUNT,
+} from "../types";
+
+export const addToCart = (product) => (dispatch, getState) => {
+  const cartItems = getState().cart.cartItems.slice();
+  let alreadyExists = false;
+  cartItems.forEach((x) => {
+    if (x.id === product.id) {
+      alreadyExists = true;
+      x.count++;
+    }
+  });
+  if (!alreadyExists) {
+    if (!product.count) {
+      product.count = 1;
+    }
+    cartItems.push(product);
+  }
+
+  dispatch({
+    type: ADD_TO_CART,
+    payload: { item: cartItems },
+  });
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+};
+
+export const removeFromCart = (product) => (dispatch, getState) => {
+  const cartItems = getState()
+    .cart.cartItems.slice()
+    .filter((x) => x.id !== product.id);
+  dispatch({ type: REMOVE_FROM_CART, payload: { cartItems } });
+  localStorage.setItem("cartItems", cartItems);
+};
+
+export function editCount(product, acum) {
+  return (dispatch) => {
+    dispatch({
+      type: UPDATE_COUNT,
+      payload: {
+        product,
+        acum,
+      },
+    });
+  };
+}
+
+export const fetchFromCart = () => (dispatch, getState) => {
+  dispatch({
+    type: FETCH_LOCAL_CART,
+    payload: { cartItems: JSON.parse(localStorage.getItem("cartItems")) },
+  });
+};
