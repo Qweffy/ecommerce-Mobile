1992a58d7f49e3bb9bee97a784fd477de92f7496
Nico Mastakas <nicolasmastakas@gmail.com>
2021-01-03 21:27:00 -0300

    parche

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -8,3 +8,18 @@ export const st = createStore(
   Reducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+// import { createStore, applyMiddleware, combineReducers } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import { cartReducer } from "./Reducers/cartReducers";
+
+// import Reducer from "./Reducers/Reducer";
+
+// export const st = createStore(
+//   combineReducers({
+//     Reducer: Reducer,
+//     cart: cartReducer,
+//   }),
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
