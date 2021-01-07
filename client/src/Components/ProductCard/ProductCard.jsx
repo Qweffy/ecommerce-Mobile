409b391d8d0539722123bddfac6e87455ca21e4e
Juan Ceschin <juanceschin@gmail.com>
2021-01-07 16:12:35 -0300

    Improved catalogue front end

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -1,16 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./ProductCard.css";
-import AddToCart from "../AddToCart/AddToCart.jsx";
-import { Link } from "react-router-dom";
-import AddToCartInvite from "../AddToCart/AddToCartInvite";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faCamera,
     faChartPie,
     faMicrochip,
 } from "@fortawesome/free-solid-svg-icons";
-import Background from "../LandingPage/Twirl__2.mp4";
-import { Container, Row, Col, Button } from "react-bootstrap";
+
 const ProductCard = ({ product }) => {
     const { name, price, img, id, stock, ram, storage, camara } = product;
     let btnDisabled = false;
@@ -18,49 +14,42 @@ const ProductCard = ({ product }) => {
     if (stock === 0) btnDisabled = true;
 
     return (
-        <div className="card">
-            <div className="flex">
-                <div className='img'>
-                    <img src={img} alt="Img not found" />
-                </div>
-                <div className="">
-                    <h3 className="">{name}</h3>
-                    <div className="">
-                        <div className="">
-                            <FontAwesomeIcon icon={faChartPie} />
-                            <div>
-                                <p >{storage}</p>
-                                <p >memoria interna</p>
-                            </div>
-                        </div>
-                        <div className="">
-                            <FontAwesomeIcon icon={faCamera} />
-                            <div>
-                                <p >{camara}</p>
-                                <p >cámara trasera</p>
-                            </div>
-                        </div>
-                        <div className="">
-                            <FontAwesomeIcon icon={faMicrochip} />
-                            <div>
-                                <p >{ram}</p>
-                                <p >Ram</p>
-                            </div>
-                        </div>
-                        <div className="">
-                            <div className="">
-                                <p>Precio final</p>
-                                <p>${price}</p>
-                            </div>
-                            <Link to={`/products/${id}`}>
-                                <button> Ver más </button>
-                            </Link>
-                        </div>
+        <div className="product-card pb-2 pt-3 px-1 m-3 d-flex">
+            <img src={img} alt="img not found" className="product-img mx-0" />
+            <div className="product-info p-2 justify-content-between mx-1 mt-1">
+                <h4>{name}</h4>
+                <div className="d-flex m-2">
+                    <FontAwesomeIcon icon={faCamera} />
+                    <div>
+                        <p className="my-0 mx-2">{camara}</p>
+                        <p className="my-0 mx-2">Camera</p>   
                     </div>
                 </div>
-            </div>
+                <div className="d-flex m-2">
+                    <FontAwesomeIcon icon={faChartPie} />
+                    <div>
+                        <p className="my-0 mx-2">{storage}</p>
+                        <p className="my-0 mx-2">Internal memory</p>   
+                    </div>
+                </div>
+                <div className="d-flex m-2">
+                    <FontAwesomeIcon icon={faMicrochip} />
+                    <div>
+                        <p className="my-0 mx-2">{ram}</p>
+                        <p className="my-0 mx-2">RAM</p>   
+                    </div>
+                </div>
+                <h5 className="product-price"><strong>${price}</strong></h5>
+            </div>            
         </div>
     );
 }
+
+// Buy button
+
+{/* <div className="mx-1 my-0">
+    <AddToCart id={id} btnDisabled={btnDisabled}/>
+    <AddToCartInvite product={product} id={id} btnDisabled={btnDisabled} />  
+</div> */}
 
 export default ProductCard;
