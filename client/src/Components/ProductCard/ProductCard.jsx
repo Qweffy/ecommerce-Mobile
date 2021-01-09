10b3e62c4bb16299b30c7e5dab8b7cf80da7745d
Nico Mastakas <nicolasmastakas@gmail.com>
2021-01-09 01:46:39 -0300

    style

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -1,83 +1,93 @@
 import React from "react";
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCamera,
-    faChartPie,
-    faMicrochip,
+  faCamera,
+  faChartPie,
+  faMicrochip,
 } from "@fortawesome/free-solid-svg-icons";
-import AddToCart from "../AddToCart/AddToCart.jsx"
-import AddToCartInvite from "../AddToCart/AddToCartInvite.jsx"
+import AddToCart from "../AddToCart/AddToCart.jsx";
+import AddToCartInvite from "../AddToCart/AddToCartInvite.jsx";
 import { useSelector } from "react-redux";
 
 const ProductCard = ({ product }) => {
-    const { name, price, img, id, stock, ram, storage, camara } = product;
-      const { user } = useSelector((state) => state.auth);
-    let btnDisabled = false;
+  const { name, price, img, id, stock, ram, storage, camara } = product;
+  const { user } = useSelector((state) => state.auth);
+  let btnDisabled = false;
 
-    if (stock === 0) btnDisabled = true;
+  if (stock === 0) btnDisabled = true;
 
-    function renderaddtocartinvite(){
-      return (
-        <div>
-          <AddToCartInvite product={product} id={id} btnDisabled={btnDisabled} />
-        </div>
-      )
-    }
-
-    function renderaddtocartuser(){
-      return (
-        <div>
-        <AddToCart id={id} btnDisabled={btnDisabled}/>
-        </div>
-      )
-    }
-
+  function renderaddtocartinvite() {
     return (
-        <div className="product-card pb-2 pt-3 px-1 d-flex">
-            <img src={img} alt="img not found" className="product-img mx-0" />
-            <div className="product-info p-2 justify-content-between mx-1 mt-1">
-                <h4>{name}</h4>
-                <div className="d-flex mx-1 my-2">
-                    <div className="d-flex align-self-center">
-                        <FontAwesomeIcon icon={faCamera} />
-                    </div>
-                    <div>
-                        <p className="product-details">{camara}</p>
-                        <p className="product-details">Camera</p>   
-                    </div>
-                </div>
-                <div className="d-flex mx-1 my-2">
-                    <div className="d-flex align-self-center">
-                        <FontAwesomeIcon icon={faChartPie} />
-                    </div>                    
-                    <div>
-                        <p className="product-details">{storage}</p>
-                        <p className="product-details">Internal memory</p>   
-                    </div>
-                </div>
-                <div className="d-flex mx-1 my-2">
-                    <div className="d-flex align-self-center">
-                        <FontAwesomeIcon icon={faMicrochip} />
-                    </div>                     
-                    <div>
-                        <p className="product-details">{ram}</p>
-                        <p className="product-details">RAM</p>   
-                    </div>
-                </div>
-                <h3 className="mx-1 mt-3 mb-2"><strong>${price}</strong></h3>
-                {user===undefined? renderaddtocartinvite() : renderaddtocartuser()}
-            </div>
-            
-        </div>
+      <div>
+        <AddToCartInvite product={product} id={id} btnDisabled={btnDisabled} />
+      </div>
     );
-}
+  }
+
+  function renderaddtocartuser() {
+    return (
+      <div>
+        <AddToCart id={id} btnDisabled={btnDisabled} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="product-card pb-2 pt-3 px-1 d-flex">
+      <img src={img} alt="img not found" className="product-img mx-0" />
+      <div className="product-info p-2 justify-content-between mx-1 mt-1">
+        <h4>{name}</h4>
+        <div className="d-flex mx-1 my-2">
+          <div className="d-flex align-self-center">
+            <FontAwesomeIcon icon={faCamera} />
+          </div>
+          <div>
+            <p className="product-details">{camara}</p>
+            <p className="product-details">Camera</p>
+          </div>
+        </div>
+        <div className="d-flex mx-1 my-2">
+          <div className="d-flex align-self-center">
+            <FontAwesomeIcon icon={faChartPie} />
+          </div>
+          <div>
+            <p className="product-details">{storage}</p>
+            <p className="product-details">Internal memory</p>
+          </div>
+        </div>
+        <div className="d-flex mx-1 my-2">
+          <div className="d-flex align-self-center">
+            <FontAwesomeIcon icon={faMicrochip} />
+          </div>
+          <div>
+            <p className="product-details">{ram}</p>
+            <p className="product-details">RAM</p>
+          </div>
+        </div>
+        <h3 className="mx-1 mt-3 mb-2">
+          <strong>${price}</strong>
+        </h3>
+        <div className="detailsss">
+          {user === undefined ? renderaddtocartinvite() : renderaddtocartuser()}
+          <Link to={`/products/${id}`}>
+            <Button>Details</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // Buy button
 
-{/* <div className="mx-1 my-0">
+{
+  /* <div className="mx-1 my-0">
     <AddToCart id={id} btnDisabled={btnDisabled}/>
     <AddToCartInvite product={product} id={id} btnDisabled={btnDisabled} />
-</div> */}
+</div> */
+}
 
 export default ProductCard;
