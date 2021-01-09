6a36af19a3568a544587e4b936eefe064b6c1efc
Nicolas Valencia <nicolasv0415@gmail.com>
2021-01-08 23:51:13 -0500

    debug cart

--- client/src/store/Reducers/cartReducers.js
+++ client/src/store/Reducers/cartReducers.js
@@ -2,27 +2,33 @@ import {
   ADD_TO_CART,
   REMOVE_FROM_CART,
   FETCH_LOCAL_CART,
+  CREATE_ORDER,
   UPDATE_COUNT,
+  UPDATE_ORDER,
+  DOWN_ORDER
 } from "../types";
 
-export const cartReducer = (
-  state = { cartItems: JSON.parse(localStorage.getItem("cartItems") || "[]") },
-  action
-) => {
+const initalOrder = {
+  cartItems: JSON.parse(localStorage.getItem("cartItems") || "[]"),
+  TotalOrden: 0
+}
+
+export const cartReducer = (state = initalOrder, action) => {
+  
   switch (action.type) {
     case ADD_TO_CART:
-
-
       return {
         ...state,
         cartItems: action.payload.item,
       };
+
     case REMOVE_FROM_CART:
-      return { cartItems: action.payload.cartItems };
+      return {...state, cartItems: action.payload.cartItems };
+
     case FETCH_LOCAL_CART:
-      return { cartItems: action.payload.cartItems };
+      return {...state,  cartItems: action.payload.cartItems };
+
     case UPDATE_COUNT:
-      console.log(action.payload);
       let ayuda = state.cartItems.slice();
       ayuda.forEach((item) => {
         if (item.id === action.payload.product.id) {
@@ -33,6 +39,20 @@ export const cartReducer = (
         ...state,
         cartItems: ayuda,
       };
+
+    case UPDATE_ORDER:
+      console.log(state.TotalOrden);
+      console.log(action.payload);
+      let changeOrder = state.TotalOrden + action.payload
+     
+      return { ...state, TotalOrden: changeOrder};
+
+    case DOWN_ORDER:
+      console.log(state.TotalOrden);
+      console.log(action.payload);
+      let changeOrderDown = state.TotalOrden - action.payload
+      return { ...state, TotalOrden: changeOrderDown};
+
     default:
       return state;
   }
