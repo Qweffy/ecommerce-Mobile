35fd751074dd5a80f9e2ec190f2a6067348bacd4
Nicolas Valencia <nicolasv0415@gmail.com>
2021-01-11 12:59:52 -0500

    Uinterface

--- client/src/Components/Login/Count/Shopping/Shoppings.jsx
+++ client/src/Components/Login/Count/Shopping/Shoppings.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const CountShoppings = () => {
+  const user = useSelector((state) => state.auth.user);
+  const [Orders, setOrders] = useState()
+
+  const getUserOrders = async ()=>{
+    let response = await axios.get(`http://localhost:4000/orders/user/${user.id}`)
+    console.log(response);
+    setOrders(response.data.data)
+  }
+
+  useEffect(()=>{
+    getUserOrders();
+  }, [])
+
+
   return ( 
-    <div>Hola desde SHOPPINGS</div>
+    <div>
+      <h1>Tus compras</h1>
+      {Orders && Orders.map(order=>{
+        return(
+          <div>cada orden</div>
+        )
+      })}
+    </div>
    );
 }
  
