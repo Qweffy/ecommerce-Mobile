9c5348b4813a40d3186630dc04f5d2dff5441327
Nicolas Valencia <nicolasv0415@gmail.com>
2021-01-11 18:52:07 -0500

    commit lunes

--- client/src/Components/Login/Count/Shopping/Shoppings.jsx
+++ client/src/Components/Login/Count/Shopping/Shoppings.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { useSelector } from "react-redux";
 import './Shopping.css'
@@ -8,9 +8,8 @@ const CountShoppings = () => {
   const [Orders, setOrders] = useState()
 
   const getUserOrders = async ()=>{
-    let response = await axios.get(`http://localhost:4000/orders/user/${user.id}`)
-    console.log(response);
-    setOrders(response.data.data)
+    let response = await axios.get(`http://localhost:4000/orders/user/${user.id}`);
+    setOrders(response.data.data);
   }
 
   useEffect(()=>{
@@ -23,9 +22,27 @@ const CountShoppings = () => {
       <h1>Tus compras</h1>
       <div className='all-orders-us'>
         {Orders && Orders.map(order=>{
+          let date = order.createdAt.slice(0, -15)
           return(
-            <div className='individual-order'>
-              <div>{order.createdAt}</div>
+            <div key={order.id} className='individual-order'>
+              <div className='order-top'>
+                <h5>Creada el {date}</h5>
+                <button> Volver a comprar </button>
+              </div>
+              <div className="order-bot">
+                {order.products.length > 0 ? 
+                  <div className='last-product'>
+                    <img src={order.products[0].img} alt="Img not found" />
+                    <div>
+                      <h5>{order.products[0].name}</h5>
+                      <p>{order.products[0].price} * { order.products[0].order_line.count } Unidad </p>
+                    </div>
+                  </div> 
+                :null} 
+                <div>
+                  <button>Ver detalles de la compra</button>
+                </div>
+              </div>
             </div>
           )
         })}
